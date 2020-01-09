4f5eeabab01f0a9427e1af64d54d526aa1d3173d
kanimisokanikani <kanimisokanikani0214@gmail.com>
2020-01-10 08:57:51 +0900

    int を使い、数字を使用し始めた 今のところくだらない文章を表示するプログラム

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/* てきとうにねりねり書いていく
+   コードだよぉ
+*/
+public class Main {
+    public static void main (String[] args) {
+        int x;
+        int y;
+        int z;
+        x = 5;
+        y = 25;
+        z = x + y;
+        System.out.println("ぼくの");
+        System.out.println("歯茎は");
+        System.out.println("膿んでいる");
+        System.out.println("ぼくは" + z + "さいです");
+        System.out.println("何か文句でもあるんですか？");
+    }
+}
