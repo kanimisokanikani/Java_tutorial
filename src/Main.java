7fca931f6d8d746ad8c581124a5ddbba2b6f95be
kanimisokanikani <kanimisokanikani0214@gmail.com>
2020-01-10 11:34:13 +0900

    サンプル制作

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,14 @@
 */
 public class Main {
     public static void main (String[] args) {
-        int x;
-        int y;
-        int z;
-        x = 5;
-        y = 25;
-        z = x + y;
-        System.out.println("ぼくの");
-        System.out.println("歯茎は");
-        System.out.println("膿んでいる");
-        System.out.println("ぼくは" + z + "さいです");
-        System.out.println("何か文句でもあるんですか？");
+        int a = 100;
+        a ++;
+        System.out.println("所持金は" + a + "円です。");
+        System.out.print("うんち！！");
+        System.out.println("ぶりゅりゅ！www");
+        int b = 10;
+        int c = 11;
+        int d = Math.max (b , c);
+        System.out.println(b + "と" + c + "を比べて、大きいのは" + d + "！！");
     }
 }
